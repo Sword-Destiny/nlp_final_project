974be44e58749b6c63985d7f7ba48fe84695ef15
天命剑主 <yuanhonglong@outlook.com>
2018-06-09 18:15:34 +0800

    initial

---
+++ final_project/src/com/sworddestiny/nlp/Main.java
@@ -0,0 +1,12 @@
+package com.sworddestiny.nlp;
+
+public class Main {
+    public static void main(String[] args){
+        if(args.length>0) {
+            String input_file = args[0];
+            System.out.println(input_file);
+        }else {
+            System.out.println("please input file!");
+        }
+    }
+}
